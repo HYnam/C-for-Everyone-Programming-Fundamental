b2bb3cb6e91e77233f1fd2f636bf1525cd754c8d
HYnam <hiuyi.nam@uqconnect.edu.au>
2022-06-20 17:51:11 +0100

    Fahrenheit to Celsius

---
+++ Week1/F_to_Celsius.c
@@ -0,0 +1,13 @@
+/* Fahrenheit to Celsius */
+#include<stdio.h>
+int main(void) {
+    int fahrenheit, celsius;
+
+    printf("Please enter fahrenheit as an integer: ");
+    scanf("%d", &fahrenheit);
+
+    celsius = (fahrenheit - 32) / 1.8;  // Conversion
+
+    printf("\n %d fahrenheit is %d celsius.\n", fahrenheit, celsius);
+    return 0;
+}
