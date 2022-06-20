382fa34b02b161efb65b041db5ff37a3b524e1b0
HYnam <hiuyi.nam@uqconnect.edu.au>
2022-06-20 18:14:27 +0100

    user input for calculating km

--- Week1/MilesTokm.c
+++ Week1/MilesTokm.c
@@ -2,7 +2,12 @@
 #include<stdio.h>   // Preprocessor for printf
 
 int main(void) {    // Program return as integer
-    int miles = 26, yards = 385;
+    // int miles = 26, yards = 385;
+    int miles, yards;
+    printf("Enter miles ");
+    scanf("%d", &miles);
+    printf("\nEnter yards ");
+    scanf("%d", &yards);
     double kilometers;
 
     kilometers = 1.609 * (miles + yards / 1760.0);
