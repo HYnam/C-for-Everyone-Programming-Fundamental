0d834a5f672bac80a7e1dcf4a8a94783aae3397f
HYnam <hiuyi.nam@uqconnect.edu.au>
2022-06-20 18:53:58 +0100

    print poem

---
+++ Week1/poem.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(void) {
+    printf("\e[1mYour Best\e[m");
+    printf("\nIf you always try your best");
+    printf("\nThen you'll never have to wonder");
+    printf("\nAbout what you could have done");
+    printf("\nIf you'd summoned all you thunder.");
+    printf("\n\nAnd if your best");
+    printf("\nWas not as good");
+    printf("\nAs you hoped it would be,");
+    printf("\n'I gave today");
+    printf("\nAll that I had in me.'");
+    return 0;
+}
