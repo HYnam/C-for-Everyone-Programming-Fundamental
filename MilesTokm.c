4c2cf84519221086e77c73e51a808115dc3321a1
HYnam <hiuyi.nam@uqconnect.edu.au>
2022-06-20 17:36:52 +0100

    Miles to km

---
+++ MilesTokm.c
@@ -0,0 +1,12 @@
+/* The distance of a marathon in kilometers */
+#include<stdio.h>   // Preprocessor for printf
+
+int main(void) {    // Program return as integer
+    int miles = 26, yards = 385;
+    double kilometers;
+
+    kilometers = 1.609 * (miles + yards / 1760.0);
+
+    printf("\nA marathon is %lf kilometers.\n\n", kilometers);
+    return 0;
+}
